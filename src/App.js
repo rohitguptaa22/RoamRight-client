4860433fc24795ee940b36c6a9a3d7db749ec8d7
rohitGupta2022 <pta.rohit28@gmail.com>
2023-02-10 21:40:57 +0530

    Added tailwind

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
-function App() {
-	return <div className='text-5xl'>Hello World</div>;
-}
+import React from "react";
+
+const App = () => {
+	return (
+		<div className=' flex items-center justify-center text-3xl font-bold underline'>
+			{/* <div className='flex items-center justify-center'></div> */}
+			Hello World
+		</div>
+	);
+};
 
 export default App;
