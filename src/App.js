981668e8de34e39837f094aa70d72ee41dc9cf00
rohitGupta2022 <pta.rohit28@gmail.com>
2023-02-14 17:12:42 +0530

    Revert "structure added"
    This reverts commit bf002e37ae5ed3cff2a47da8f6af39e0d35fdf9b.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,5 @@
-<<<<<<< Updated upstream
 function App() {
 	return <div className='text-5xl'>Hello World</div>;
 }
-=======
-import React from "react";
-import { Header, Footer } from "./Layouts/Home";
-import Home from "./components/Home";
-import { CssBaseline, Box } from "@mui/material";
-
-const App = () => {
-	return (
-		// <div className=' flex items-center justify-center text-3xl font-bold underline'>
-		<div className='p-4'>
-			<Box className='bg-gray-300 h-full '>
-				<Home />
-			</Box>
-		</div>
-	);
-};
->>>>>>> Stashed changes
 
 export default App;
--- src/Layouts/Home/Footer.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Footer = () => {
-	return <div className='h-2 w-2'>Footer</div>;
-};
-
-export default Footer;
